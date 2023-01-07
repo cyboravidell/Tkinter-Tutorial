b0bbc82764a7303727108fc2893e6b8d62c3e8a5
Ravi kumar <cyboravidell@gmail.com>
2023-01-08 02:38:21 +0530

    Adding Widget part:1 --> left icon created

--- contacts.py
+++ contacts.py
@@ -3,19 +3,13 @@ from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry
 from tkinter import ttk    # Provides access to the Tk themed widgets.
 
 class Contacts:
-    def __init__(self,root):
+    def __init__(self):
         self.root = root
-        self.create_left_icon()
-
+    
     def create_left_icon(self):
-        photo = PhotoImage(file='E:\Tkinter Tutorial\Contacts\icons\logo.gif')
-        label = Label(image=photo)
-        label.image = photo
-        label.grid(row=0,column=0)
-
+        photo = PhotoImage(file = 'icons/logo.gif')
 
 if __name__ == '__main__':
     root  = Tk()
     root.title('My Contacts List')
-    application = Contacts(root)
     root.mainloop()
