f2a032169c80b2a6bb80548ae53725288185dcfd
Ravi kumar <cyboravidell@gmail.com>
2023-01-08 02:02:39 +0530

    Creating a class (Contacts)

---
+++ Contacts/contacts.py
@@ -0,0 +1,12 @@
+# import what you need from tkinter module
+from tkinter import Tk, Button, PhotoImage, Label, LabelFrame, W, E, N, S, Entry, END, StringVar, Scrollbar, Toplevel
+from tkinter import ttk    # Provides access to the Tk themed widgets.
+
+class Contacts:
+    def __init__(self):
+        self.root = root
+
+if __name__ == '__main__':
+    root  = Tk()
+    root.title('My Contacts List')
+    root.mainloop()
